7e4323b5ed0b9ee2af873cafd6d928de010773e0
iamjohnpalencia <47620043+iamjohnpalencia@users.noreply.github.com>
2022-01-27 23:50:38 +0800

    C
    C

--- SQL/Latest DB/pos.sql
+++ SQL/Latest DB/pos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 26, 2022 at 05:04 AM
+-- Generation Time: Jan 27, 2022 at 03:10 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.7
 
